bf1180468c5ae649cc84315f4a21862e11fc1ef7
duong <nguyenxuanduong1804@gmail.com>
2023-09-27 19:56:05 +0700

    #27

--- src/route/web.js
+++ src/route/web.js
@@ -5,10 +5,11 @@ let router = express.Router();
 
 let initWebRoutes = (app) => {
     router.get('/', homeController.getHomePage);
+    router.get('/about', homeController.getAboutPage);
+    router.get('/crud', homeController.getCRUD);
 
-    router.get('/hoidanit', (req, res)=>{
-        return res.send("Hello world with hoidanit");
-    });
+    router.post('/post-crud', homeController.postCRUD);
+    router.get('/get-crud', homeController.displayGetCRUD);
 
     //rest api
 
