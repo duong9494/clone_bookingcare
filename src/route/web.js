9524943a8c82ec67980b1d673fd375dce922cd1f
duong <nguyenxuanduong1804@gmail.com>
2023-09-30 18:11:11 +0700

    video api login

--- src/route/web.js
+++ src/route/web.js
@@ -1,5 +1,6 @@
 import express from "express";
 import homeController from "../controllers/homeController";
+import userController from "../controllers/userController"
 
 let router = express.Router();
 
@@ -10,8 +11,12 @@ let initWebRoutes = (app) => {
 
     router.post('/post-crud', homeController.postCRUD);
     router.get('/get-crud', homeController.displayGetCRUD);
+    router.get('/edit-crud', homeController.getEditCRUD);
+    
+    router.post('/put-crud', homeController.putCRUD);
+    router.get('/delete-crud', homeController.deleteCRUD);
 
-    //rest api
+    router.post('/api/login', userController.handleLogin);
 
     return app.use("/", router);
 }
