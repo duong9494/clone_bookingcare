725c88edf2058e2092dc3e7129850a62dbce12e5
duong <nguyenxuanduong1804@gmail.com>
2023-10-16 19:07:20 +0700

    #59 create user with redux

--- src/services/userService.js
+++ src/services/userService.js
@@ -100,36 +100,37 @@ let getAllUsers = (userId) => {
 }
 
 let createNewUser = (data) => {
-    return new Promise(async(resolve, reject) => {
-        try {
-            // check email is exist ???
-            let check = await checkUserEmail(data.email)
-            if (check === true) {
-                resolve({
-                    errCode: 1,
-                    errMessage: 'Your email is already in used, Plz try another email'
-                })
-            } else {
-                let hashPasswordFromBcrypt = await hashUserPassword(data.password)
-                await db.User.create({
-                    email: data.email,
-                    password: hashPasswordFromBcrypt,
-                    firstName: data.firstName,
-                    lastName: data.lastName,
-                    address: data.address,
-                    phonenumber: data.phonenumber,
-                    gender: data.gender === '1' ? true : false,
-                    roleId: data.roleId,
-                })
-                resolve({
-                    errCode: 0,
-                    message: 'Ok'
-                })
-            }
-        } catch (e) {
-            reject(e)
-        }
-    })
+  return new Promise(async(resolve, reject) => {
+    try {
+      // check email is exist ???
+      let check = await checkUserEmail(data.email)
+      if (check === true) {
+        resolve({
+          errCode: 1,
+          errMessage: 'Your email is already in used, Plz try another email'
+        })
+      } else {
+        let hashPasswordFromBcrypt = await hashUserPassword(data.password)
+        await db.User.create({
+          email: data.email,
+          password: hashPasswordFromBcrypt,
+          firstName: data.firstName,
+          lastName: data.lastName,
+          address: data.address,
+          phonenumber: data.phonenumber,
+          gender: data.gender,
+          roleId: data.roleId,
+          positionId: data.positionId
+        })
+        resolve({
+          errCode: 0,
+          message: 'Ok'
+        })
+      }
+    } catch (e) {
+      reject(e)
+    }
+  })
 }
 
 let deleteUser = (userId) => {
